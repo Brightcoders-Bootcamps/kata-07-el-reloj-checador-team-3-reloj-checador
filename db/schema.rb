5902244f79fbfce4133be3efe91f68fa36898f5b
Karina Delgado <kdelgado@ucol.mx>
2020-11-01 22:04:17 -0600

    Basic auth for admin - missing style and menu
    Co-authored-by: danielasotomayor99 <danysotomayor99@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_192147) do
+ActiveRecord::Schema.define(version: 2020_11_02_021904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
     t.string "user"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
