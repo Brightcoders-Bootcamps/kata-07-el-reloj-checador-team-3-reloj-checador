9890896e4ea43b3f5e4366a07abd8f87329a77e4
Karina Delgado <kdelgado@ucol.mx>
2020-11-01 22:04:17 -0600

    Basic auth for admin - missing style and menu
    Co-authored-by: danielasotomayor99 <danysotomayor99@gmail.com>

--- db/migrate/20201029171500_create_admins.rb
+++ db/migrate/20201029171500_create_admins.rb
@@ -2,7 +2,7 @@ class CreateAdmins < ActiveRecord::Migration[6.0]
   def change
     create_table :admins do |t|
       t.string :user
-      t.string :password
+      t.string :password_digest
 
       t.timestamps
     end
