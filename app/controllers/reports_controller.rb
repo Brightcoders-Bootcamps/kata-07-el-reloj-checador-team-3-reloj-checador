e78b01b87fd14e6d6178c6ef1342f1f9f922fb12
ediazjz <ediazjz@outlook.com>
2020-11-05 09:44:53 -0600

    Add new report view - Changes in Gemfile to work locally - Corrected seed - Index report view prototype - Add Layout App

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,29 @@
 class ReportsController < ApplicationController
   def index
+    @reports = Report.all
   end
 
   def show
   end
 
   def new
+    @report = Report.new
   end
 
   def edit
+  end
+
+  def create
+    @report = Report.new
+
+    respond_to do |format|
+      if @report.save
+        format.html { redirect_to @report, notice: 'Report was successfully created.' }
+        format.json { render :show, status: :created, location: @report }
+      else
+        format.html { render :new }
+        format.json { render json: @report.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,4 @@
 class Report < ApplicationRecord
-belongs_to :employee
-enum check_type: [:check_in, :check_out]
+  belongs_to :employee
+  enum check_type: [:check_in, :check_out]
 end
