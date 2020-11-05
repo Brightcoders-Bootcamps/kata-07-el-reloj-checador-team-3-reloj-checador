b52f4298f45f960b326d6ed92f6028bd6d892f97
Karina Delgado <kdelgado@ucol.mx>
2020-11-05 14:20:00 -0600

    Changes requested about admin controller done
    Co-authored-by: danielasotomayor99 <danysotomayor99@gmail.com>

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,16 +1,3 @@
 class AdminsController < ApplicationController
 
-  skip_before_action :authorized, only: [:new, :create]
-
-  def new
-    @admin = Admin.new
-  end
-
-  def create
-    @admin = Admin.create(params.require(:admin).permit(:user,        
-    :password))
-    session[:admin_id] = @admin.id
-    redirect_to '/welcome'
- 
-  end
 end
