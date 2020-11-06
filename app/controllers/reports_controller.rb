5afb391c93f9257c6ef9f1dc1d85ce2c2bf78f0d
ediazjz <ediazjz@outlook.com>
2020-11-06 17:15:33 -0600

    Add New Time Registration and Formatting

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -26,7 +26,7 @@ class ReportsController < ApplicationController
     
     respond_to do |format|
       if @report.save
-        format.html { redirect_to @report, notice: 'Report was successfully created.' }
+        format.html { redirect_to new_report_path, notice: 'Report was successfully created.' }
       else
         format.html { render :new }
       end
