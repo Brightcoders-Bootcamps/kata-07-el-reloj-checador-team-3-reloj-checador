52309059a627612fa5dade01adf0befc2efef3e4
Karina Delgado <kdelgado@ucol.mx>
2020-11-06 10:45:33 -0600

    Review comments applied
    Co-authored-by: danielasotomayor99 <danysotomayor@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
   get 'authorized', to: 'sessions#page_requires_login'
-  get 'logout', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
