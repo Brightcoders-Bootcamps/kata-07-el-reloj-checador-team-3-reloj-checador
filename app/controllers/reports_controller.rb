567a6d88c8df1cba7e9e15d005162bd723ea04c1
ediazjz <ediazjz@outlook.com>
2020-11-06 10:39:00 -0600

    Add New Realion Employee:Report for Checks

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,10 +19,8 @@ class ReportsController < ApplicationController
     respond_to do |format|
       if @report.save
         format.html { redirect_to @report, notice: 'Report was successfully created.' }
-        format.json { render :show, status: :created, location: @report }
       else
         format.html { render :new }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
       end
     end
   end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,6 @@
 class Report < ApplicationRecord
   belongs_to :employee
   enum check_type: [:check_in, :check_out]
+
+  @private_number 
 end
