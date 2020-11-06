c605afb12b73133427ac43d96653335005be564d
ediazjz <ediazjz@outlook.com>
2020-11-06 16:58:56 -0600

    Add Check-in/Check-out options

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -16,8 +16,14 @@ class ReportsController < ApplicationController
   def create
     related_employee = Employee.find_by(private_number: params["report"]["private_number"].to_i)
     @report = Report.new
-    @report.employee =  related_employee
 
+    if params[:commit] == 'Check-in'
+      @report.check = 'check in'
+    elsif params[:commit] == 'Check-out'
+      @report.check = 'check out'
+    end
+    @report.employee =  related_employee
+    
     respond_to do |format|
       if @report.save
         format.html { redirect_to @report, notice: 'Report was successfully created.' }
