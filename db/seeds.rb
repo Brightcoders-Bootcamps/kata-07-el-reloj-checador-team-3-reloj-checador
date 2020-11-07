737a39f7035a3e542022d7bdc9e0904650436664
Karina Delgado <kdelgado@ucol.mx>
2020-11-07 17:20:07 -0600

    Content commented for test
    Co-authored-by: danielasotomayor99 <danysotomayor99@gmail.com>
    Co-authored-by: Edgar Diaz <ediazjz@outlook.com>
    Co-authored-by: EduJimenez96 <ejimenez10@ucol.mx>

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Employee.create(name: 'Karina Delgado', email: 'karina@hotmail.com', position: 'QA', private_number: 123, status: 'active')
-Employee.create(name: 'Daniela Sotomayor', email: 'dany@hotmail.com', position: 'Developer', private_number: 321, status: 'active')
-Admin.create(user: 'Karina', password_digest: '123')
-Branch.create(name: 'HR', address: '3rd Floor')
-Report.create(check: 'check in', check_time: "2016-11-01 17:00:13", employee_id: "1")
+# Employee.create(name: 'Karina Delgado', email: 'karina@hotmail.com', position: 'QA', private_number: 123, status: 'active')
+# Employee.create(name: 'Daniela Sotomayor', email: 'dany@hotmail.com', position: 'Developer', private_number: 321, status: 'active')
+# Admin.create(user: 'Karina', password_digest: '123')
+# Branch.create(name: 'HR', address: '3rd Floor')
+# Report.create(check: 'check in', check_time: "2016-11-01 17:00:13", employee_id: "1")
