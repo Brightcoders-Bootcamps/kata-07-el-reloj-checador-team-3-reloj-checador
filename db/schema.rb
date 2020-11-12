c720fffb2de4633b4ef086319cbecb84607448dc
EduJimenez96 <ejimenez10@ucol.mx>
2020-11-12 12:10:58 -0600

    Changing data type from migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_193814) do
+ActiveRecord::Schema.define(version: 2020_11_12_180413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
